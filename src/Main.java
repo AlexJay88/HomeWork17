a0759f13d7130e18c901c39a755ee26d9a9e5add
alex <jason33@yandex.ru>
2023-04-24 17:21:54 +0300

    dz17

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        String login="login123";
+        String password="password_0";
+        String confirmPassword="password_0";
+
+
+        Validator.validate(login, password, confirmPassword);
+
+}}
+
---
+++ src/Validator.java
@@ -0,0 +1,23 @@
+import java.util.regex.Pattern;
+
+public class Validator {
+    public static void validate(
+            String login,
+            String password,
+            String confirmPassword
+    ){
+
+        if(!Pattern.matches("^[A-Za-z0-9_]{1,20}$",login)){
+            throw new WrongLoginException("Неправильный логин");
+        }
+        if(!Pattern.matches("^[A-Za-z0-9_]{1,19}$",password)){
+            throw new WrongPasswordException("Неправильный пароль");
+        }
+        if(!password.equals(confirmPassword)){
+            throw new WrongPasswordException("ошибка-неверный пароль");
+        }
+    }}
+
+
+
+
