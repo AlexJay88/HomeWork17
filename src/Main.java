82bb8a5e4da50a6e4e174afb5bc662fa3f17f279
alex <jason33@yandex.ru>
2023-04-25 11:17:08 +0300

    dz17

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,19 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        String login="login123";
-        String password="password_0";
-        String confirmPassword="password_0";
+        String login = "login123";
+        String password = "password_0";
+        String confirmPassword = "password_0";
 
+        try {
+            Validator.validate(login, password, confirmPassword);
 
-        Validator.validate(login, password, confirmPassword);
+        } catch (WrongLoginException | WrongPasswordException exception) {
+            exception.printStackTrace();
+        }finally {
+            System.out.println("finally блок");
+        }
 
-}}
+    }}
 
 
